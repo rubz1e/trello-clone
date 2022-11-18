07b2174a1934360af456107a8a966308b0fa39de
AngelinaKostyukovets <110695442+AngelinaKostyukovets@users.noreply.github.com>
2022-11-18 21:52:25 +0300

    init: task move function and buttons events

--- src/components/tasks/index.js
+++ src/components/tasks/index.js
@@ -1,17 +1,99 @@
 import createElement from "../../utils/createElement.js";
 import findFreeId from "../../utils/findFreeId.js";
-import Task from "../tasks/taskCreated.js"
+import Task from "../tasks/taskCreated.js";
 
-const tasks = {
-    element: createElement('ul', {className: 'todo-item'}),
-    list: [],
-    addTask(title, description, user, date) {
-        const id = findFreeId(this.list)
-        const task = new Task(id, title, description, user, date);
-        this.list.push(task);
-        this.element.append(task.element);
-        console.log(this)
-    }
+function changeButtons(task, nameFirstBtn, nameSecondBtn) {
+  task.taskArrow.classList.toggle("task-mised");
+  task.taskEdit.textContent = nameFirstBtn;
+  task.taskDelete.textContent = nameSecondBtn;
 }
 
-export default tasks;
+const tasksDone = {
+  element: createElement("ul", { className: "done-item" }),
+  list: [],
+  addTask(task) {
+    const id = findFreeId(this.list);
+    task.id = id;
+    this.list.push(task);
+    this.element.append(task.element);
+
+    task.taskEdit.classList.toggle("task-mised");
+    task.taskDelete.textContent = "Удалить";
+
+    task.taskDelete.addEventListener("click", () => {
+      const deleteTask = this.list.find((task) => task.id === id);
+      if (deleteTask) {
+        task.element.remove();
+        this.list = this.list.filter((task) => task.id !== id);
+      }
+    });
+  },
+};
+
+const tasksInProgress = {
+  element: createElement("ul", { className: "inProgress-item" }),
+  list: [],
+  addTask(task) {
+    const id = findFreeId(this.list);
+    task.id = id;
+    this.list.push(task);
+    this.element.append(task.element);
+
+    changeButtons(task, "Вернуть", "Завершить");
+
+    task.taskDelete.addEventListener("click", () => {
+      // дублирование, придумать куда вынести
+      const movedTask = this.list.find((task) => task.id === id);
+      if (movedTask) {
+        tasksDone.addTask(task);
+        this.list = this.list.filter((task) => task.id !== id);
+      }
+    });
+
+    task.taskEdit.addEventListener("click", () => {
+      // дублирование
+      const movedTask = this.list.find((task) => task.id === id);
+      if (movedTask) {
+        tasks.returnTask(task);
+        this.list = this.list.filter((task) => task.id !== id);
+      }
+    });
+  },
+};
+
+const tasks = {
+  element: createElement("ul", { className: "todo-item" }),
+  list: [],
+  addTask(title, description, user, date) {
+    const id = findFreeId(this.list);
+    const task = new Task(id, title, description, user, date);
+    this.list.push(task);
+    this.element.append(task.element);
+
+    task.taskArrow.addEventListener("click", () => {
+      const movedTask = this.list.find((task) => task.id === id);
+      if (movedTask) {
+        tasksInProgress.addTask(task);
+        this.list = this.list.filter((task) => task.id !== id);
+      }
+    });
+
+    task.taskDelete.addEventListener("click", () => {
+      // вынести в функцию?
+      const deleteTask = this.list.find((task) => task.id === id);
+      if (deleteTask) {
+        task.element.remove();
+        this.list = this.list.filter((task) => task.id !== id);
+      }
+    });
+  },
+  returnTask(task) {
+    const id = findFreeId(this.list);
+    task.id = id;
+    this.list.push(task);
+    this.element.append(task.element);
+    changeButtons(task, "Редактировать", "Удалить");
+  },
+};
+
+export { tasksDone, tasksInProgress, tasks };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import createElement from "./utils/createElement.js"
-import Task from './components/tasks/taskCreated.js'
-import tasks from './components/tasks/index.js'
+import createElement from "./utils/createElement.js";
+import Task from "./components/tasks/taskCreated.js";
+import { tasksInProgress, tasks, tasksDone } from "./components/tasks/index.js";
 
 const createOfDate = new Date().toLocaleString();
-tasks.addTask('title', '123awdwadwadwadawda', 'Angelina Sergeevna', createOfDate)
+tasks.addTask(
+  "title",
+  "123awdwadwadwadawda",
+  "Angelina Sergeevna",
+  createOfDate
+);
 
-document.querySelector('#todo-list')?.append(tasks.element)
+document.querySelector(".columns-todo > .columns-button").before(tasks.element);
+document.querySelector(".columns-inprogress").append(tasksInProgress.element);
+document.querySelector(".columns-done > .columns-button").before(tasksDone.element);
